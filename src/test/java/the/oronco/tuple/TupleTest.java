2a7cb2b9e64c4648f77b358cf40a2f83de4e6354
Theo Roncoletta <theo.roncoletta@atr-software.de>
2024-03-14 21:55:09 +0100

    some more tuple tests

--- src/test/java/the/oronco/tuple/TupleTest.java
+++ src/test/java/the/oronco/tuple/TupleTest.java
@@ -2,6 +2,9 @@ package the.oronco.tuple;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.function.Executable;
@@ -12,6 +15,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 import the.oronco.adt.Option;
 import the.oronco.adt.Result;
 
+import java.util.List;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -28,10 +32,9 @@ class TupleTest {
 
     @ParameterizedTest
     @MethodSource("tuplesWithSizes")
-    void testGet(Tuple tuple, int size) {
-
+    void testGetSuccess(Tuple tuple, int size) {
         assertAll(IntStream.range(0, size)
-                           .mapToObj(i -> (Executable) () -> {
+                           .<Executable>mapToObj(i -> () -> {
                                var result = tuple.get(i);
                                switch (result) {
                                    case Result.Ok<Option<Object>, TupleError.IndexError>(var value) -> value.inspect(v -> assertEquals(i, v));
@@ -42,19 +45,62 @@ class TupleTest {
                            .toList());
     }
 
+    @ParameterizedTest
+    @MethodSource("tuplesWithSizes")
+    void testGetTooLarge(Tuple tuple) {
+        Result<Option<Object>, TupleError.IndexError> actualValue = tuple.get(Integer.MAX_VALUE);
+        switch (actualValue) {
+            case Result.Err<Option<Object>, TupleError.IndexError>(var error) -> assertInstanceOf(TupleError.IndexError.IndexTooLargeError.class,
+                                                                                                  error);
+            case Result.Ok<Option<Object>, TupleError.IndexError>(var ignored) -> fail("The tuple should not have returned anything here!");
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("tuplesWithSizes")
+    void testGetTooSmall(Tuple tuple) {
+        Result<Option<Object>, TupleError.IndexError> actualValue = tuple.get(-1);
+        switch (actualValue) {
+            case Result.Err<Option<Object>, TupleError.IndexError>(var error) -> assertInstanceOf(TupleError.IndexError.IndexSmallerZeroError.class,
+                                                                                                  error);
+            case Result.Ok<Option<Object>, TupleError.IndexError>(var ignored) -> fail("The tuple should not have returned anything here!");
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("tuplesWithSizes")
+    void testContains(Tuple tuple, int size) {
+        assertAll(IntStream.range(0, size)
+                           .boxed()
+                           .<Executable>flatMap(i -> Stream.of(() -> assertTrue(tuple.contains(i)),
+                                                               () -> assertFalse(tuple.contains(Integer.MAX_VALUE))))
+                           .toList());
+    }
+
+    @ParameterizedTest
+    @MethodSource("tuplesWithSizes")
+    void testContainsAllIterable(Tuple tuple, int size) {
+        var stuff = IntStream.range(0, size);
+        assertAll(() -> assertTrue(tuple.containsAll(List.of())),
+                  () -> assertTrue(tuple.containsAll(stuff.boxed()
+                                                          .toList())),
+                  () -> assertFalse(tuple.containsAll(IntStream.range(0, size + 1)
+                                                               .boxed()
+                                                               .toList())));
+    }
+
     @ValueSource
     static Stream<Arguments> tuplesWithSizes() {
-        return Stream.of(
-                Arguments.of(Empty.of(), 0),
-                Arguments.of(Unit.of(0), 1),
-                Arguments.of(Pair.of(0, 1), 2),
-                Arguments.of(Triplet.of(0, 1, 2), 3),
-                Arguments.of(Quartet.of(0, 1, 2, 3), 4),
-                Arguments.of(Quintet.of(0, 1, 2, 3, 4), 5),
-                Arguments.of(Sextet.of(0, 1, 2, 3, 4, 5), 6),
-                Arguments.of(Septet.of(0, 1, 2, 3, 4, 5, 6), 7),
-                Arguments.of(Octet.of(0, 1, 2, 3, 4, 5, 6, 7), 8),
-                Arguments.of(Ennead.of(0, 1, 2, 3, 4, 5, 6, 7, 8), 9),
-                Arguments.of(Decade.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9), 10));
+        return Stream.of(Arguments.of(Empty.of(), 0),
+                         Arguments.of(Unit.of(0), 1),
+                         Arguments.of(Pair.of(0, 1), 2),
+                         Arguments.of(Triplet.of(0, 1, 2), 3),
+                         Arguments.of(Quartet.of(0, 1, 2, 3), 4),
+                         Arguments.of(Quintet.of(0, 1, 2, 3, 4), 5),
+                         Arguments.of(Sextet.of(0, 1, 2, 3, 4, 5), 6),
+                         Arguments.of(Septet.of(0, 1, 2, 3, 4, 5, 6), 7),
+                         Arguments.of(Octet.of(0, 1, 2, 3, 4, 5, 6, 7), 8),
+                         Arguments.of(Ennead.of(0, 1, 2, 3, 4, 5, 6, 7, 8), 9),
+                         Arguments.of(Decade.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9), 10));
     }
 }
